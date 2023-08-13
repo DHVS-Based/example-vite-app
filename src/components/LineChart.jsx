32ccdaded0d111515d3f26524e7622d158f7f5e9
DHVS-Based <diego@based.so>
2023-08-13 10:59:38 -0600

    LineChart to function component

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -46,32 +46,30 @@ const data = [
   },
 ];
 
-class LineChartComp extends PureComponent {
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeDasharray="5 5" />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
-        </LineChart>
-      </ResponsiveContainer>
-    );
-  }
+const LineChartComp = () => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeDasharray="5 5" />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 }
 
 export default LineChartComp;
