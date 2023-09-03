657ab45dbe5123e5f3851f962ebde378f31c32c5
DHVS-Based <diego@based.so>
2023-09-03 11:41:55 -0600

    Shadcn added to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import SideMenu from './components/SideMenu'
 import Navbar from './components/Navbar'
 import Dashboard from './components/Dashboard'
+import { Button } from './../components/ui/button'
+import { ThemeProvider, useTheme } from '@/components/theme-provider'
 
 function App() {
   const [count, setCount] = useState(0)
+  const { setTheme } = useTheme();
+  setTheme('light');
 
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <div className='flex min-h-screen'>
-        <SideMenu />
-        <section className='flex flex-col grow'>
-          <Navbar />
-          <Dashboard />
-        </section>
-      </div>
-    </main>
+    <ThemeProvider defaultTheme="light" storageKey='theme'>
+      <main className='bg-gray-100 min-h-screen w-screen'>
+        <div className='flex min-h-screen'>
+          <SideMenu />
+          <section className='flex flex-col grow'>
+            <Navbar />
+            <Dashboard />
+          </section>
+        </div>
+      </main>
+    </ThemeProvider>
   )
 }
 
